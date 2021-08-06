32699d8e66508576aec9466321f74d4c7f861b01
Vinicius Pacheco <pachecovinicius18@gmail.com>
2021-08-06 15:48:00 -0300

    Adicionando get na controller Usuario

--- src/main/java/br/org/generation/blogpessoal/controller/UsuarioController.java
+++ src/main/java/br/org/generation/blogpessoal/controller/UsuarioController.java
@@ -1,11 +1,14 @@
 package br.org.generation.blogpessoal.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.org.generation.blogpessoal.model.UsuarioLoginModel;
 import br.org.generation.blogpessoal.model.UsuarioModel;
+import br.org.generation.blogpessoal.repository.UsuarioRepository;
 import br.org.generation.blogpessoal.service.UsuarioService;
 
 @RestController
@@ -23,6 +27,19 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioService usuarioService;
+	private UsuarioRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<UsuarioModel>> getAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UsuarioModel> getById(@PathVariable long id) {
+		return repository.findById(id)
+		.map(resp -> ResponseEntity.ok(resp))
+		.orElse(ResponseEntity.notFound().build());
+	}
 	
 	@PostMapping("/logar")
 	public ResponseEntity<UsuarioLoginModel> Autentication (@RequestBody Optional<UsuarioLoginModel> user) {
